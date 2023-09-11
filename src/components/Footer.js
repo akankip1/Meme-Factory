d8c22b69ffea45030c4c49d468c186068ac13a4e
Ashrit Kankipati <venkat.ashrit@gmail.com>
2023-09-11 03:25:05 -0400

    working code

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Created with &#128293; by &nbsp;
+                <a className="website-link" href="">
+                    Ashrit 
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
